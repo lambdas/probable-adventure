25b05a432bd61fcefa5558cb0d0431ccb7995756
Pavel Logvinov <lambdas@pm.me>
2021-06-06 14:34:55 +0200

    Add an application launcher

---
+++ src/main/scala/lambdas/challenge/App.scala
@@ -0,0 +1,61 @@
+package lambdas.challenge
+
+import akka.Done
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.Merge
+import akka.stream.scaladsl.Source
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import sun.misc.Signal
+import sun.misc.SignalHandler
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.jdk.CollectionConverters._
+import scala.util.Failure
+
+object App {
+
+  private implicit val system: ActorSystem = ActorSystem("challenge")
+  private implicit val ec: ExecutionContext = system.dispatcher
+
+  private val config = ConfigFactory.load()
+
+  def main(args: Array[String]): Unit = {
+    initShutdownHook()
+
+    WordCount
+      .source(readers())
+      .via(WordCount.flow(tickSource()))
+      .runWith(WordCount.sink)
+      .andThen { case _ => system.terminate() }
+  }
+
+  private def readers(): List[SlowCharacterReader] = {
+    val minDelay = FiniteDuration(config.getDuration("reader-delay.min").toMillis, TimeUnit.MILLISECONDS)
+    val maxDelay = FiniteDuration(config.getDuration("reader-delay.max").toMillis, TimeUnit.MILLISECONDS)
+
+    config
+      .getStringList("readers")
+      .asScala
+      .map(new SlowCharacterReader(_, minDelay, maxDelay))
+      .toList
+  }
+
+  private def tickSource(): Source[Unit, NotUsed] = {
+    val interval = FiniteDuration(config.getDuration("report-interval").toMillis, TimeUnit.MILLISECONDS)
+
+    Source
+      .tick(interval, interval, ())
+      .mapMaterializedValue(_ => NotUsed)
+  }
+
+  private def initShutdownHook(): Unit = {
+    Signal
+      .handle(new Signal("INT"), _ => Await.result(system.terminate(), 5.minutes))
+  }
+}
